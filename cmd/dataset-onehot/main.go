ec7e25a9f6eb6b1a6b653a5dea8a0e9a455af477
Hugo Devillers <hugo.devillers@gmail.com>
2021-02-22 15:57:19 +0100

    Manager training, testing and validation set.

--- cmd/dataset-onehot/main.go
+++ cmd/dataset-onehot/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"strconv"
 
-	"gonum.org/v1/hdf5"
 	"github.com/hdevillers/go-dl-seq/dataset"
-	"github.com/hdevillers/go-dl-seq/seqio"
-	"github.com/hdevillers/go-dl-seq/seq"
 	"github.com/hdevillers/go-dl-seq/encoding"
+	"github.com/hdevillers/go-dl-seq/seq"
+	"github.com/hdevillers/go-dl-seq/seqio"
+	"gonum.org/v1/hdf5"
 )
 
 func check(e error) {
@@ -17,24 +18,15 @@ func check(e error) {
 	}
 }
 
-func main() {
-	// Retrieve argument values
-	input := flag.String("input", "", "Input sequence file.")
-	format := flag.String("format", "fasta", "Input format.")
-	output := flag.String("output", "dataset.h5", "Output dataset file (h5).")
-	flag.Parse()
-
-	if *input == "" {
-		panic("You must provide an input fasta file.")
-	}
-	
-	// Read input sequences
+func fastaToDataset(input string, format string, h5 *hdf5.File, dtype string) (int, int) {
+	// Initialize variables
 	var seqs []seq.Seq
-	var trainLabels []string
-	var trainY []uint16
+	var labels []string
+	var y []uint16
 
+	// Open input fasta file an scan it
 	slen := 0
-	seqIn := seqio.NewReader(*input, *format)
+	seqIn := seqio.NewReader(input, format)
 	seqIn.CheckPanic()
 	defer seqIn.Close()
 	for seqIn.Next() {
@@ -42,10 +34,10 @@ func main() {
 		// Initialize the encoder
 		s := seqIn.Seq()
 		seqs = append(seqs, s)
-		trainLabels = append(trainLabels, s.Id)
+		labels = append(labels, s.Id)
 		tmpY, err := strconv.Atoi(s.Desc)
 		check(err)
-		trainY = append(trainY, uint16(tmpY))
+		y = append(y, uint16(tmpY))
 
 		// Throw an error if lengths differ between sequences
 		if slen == 0 {
@@ -59,22 +51,64 @@ func main() {
 	nseq := len(seqs)
 
 	// Prepare OneHot coding data
-	trainX := make([]uint16, nseq * slen * 4)
-	for i,s := range seqs {
+	x := make([]uint16, nseq*slen*4)
+	for i, s := range seqs {
 		oho := encoding.NewOneHot(s.Sequence)
 		oho.Compute()
 		oho.CheckPanic()
 		at := slen * 4 * i
-		copy(trainX[at:], oho.GetOho())
+		copy(x[at:], oho.GetOho())
+	}
+
+	// Add the data set to the h5 file
+	err := dataset.AddOhoDataset(h5, dtype, x, y, labels, nseq, slen)
+	check(err)
+
+	return slen, nseq
+}
+
+func main() {
+	// Retrieve argument values
+	training := flag.String("train", "", "Input training sequence file.")
+	testing := flag.String("test", "", "Input testing sequence file.")
+	validation := flag.String("valid", "", "Input validation sequence file.")
+	format := flag.String("format", "fasta", "Input format.")
+	output := flag.String("output", "dataset.h5", "Output dataset file (h5).")
+	flag.Parse()
+
+	if *training == "" {
+		panic("You must provide an input training file.")
 	}
 
 	// Create the H5 file
 	h5, err := hdf5.CreateFile(*output, hdf5.F_ACC_TRUNC)
 	check(err)
 
-	// Save training dataset
-	dataset.AddOhoDataset(h5, "training", trainX, trainY, trainLabels, nseq, slen)
+	// Generate the training dataset
+	ltrain, ntrain := fastaToDataset(*training, *format, h5, "training")
+	fmt.Printf("Training set: %d sequences of %d bases.\n", ntrain, ltrain)
 
-	
+	// Generate the testing dataset
+	if *testing != "" {
+		ltest, ntest := fastaToDataset(*testing, *format, h5, "testing")
+		if ltest != ltrain {
+			panic("Training and testing sequences must have the same length.")
+		}
+		fmt.Printf("Testing set: %d sequences of %d bases.\n", ntest, ltest)
+	} else {
+		fmt.Println("No testing set.")
+	}
+
+	// Generate the validation dataset
+	if *validation != "" {
+		lvalid, nvalid := fastaToDataset(*validation, *format, h5, "validation")
+		if lvalid != ltrain {
+			panic("Training and validation sequences must have the same length.")
+		}
+		fmt.Printf("Validation set: %d sequences of %d bases.\n", nvalid, lvalid)
+	} else {
+		fmt.Println("No validation set.")
+	}
+
 	h5.Close()
 }
