45b419bb40147fa8be840b16f457a4cacec3a3ab
Hugo Devillers <hugo.devillers@gmail.com>
2021-05-05 22:20:33 +0200

    Delete useless interfaces.

--- kmer/kmer.go
+++ kmer/kmer.go
@@ -7,17 +7,6 @@ const (
 	MaxKAbsolute int = 31
 )
 
-type KmerManager interface {
-	Init([]byte)
-	AddBase(byte)
-}
-
-type KmerSlice interface {
-	Len() int
-	Less(int, int)
-	Swap(int, int)
-}
-
 type KmerCounter interface {
 	Count([]byte)
 	Finish()
