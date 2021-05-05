50138e023a3ebc507f5eab71031bbb3de9802c19
Hugo Devillers <hugo.devillers@gmail.com>
2021-05-05 17:44:19 +0200

    Add KmerManager, KmerSlice and KmerCounter interface.

--- kmer/kmer.go
+++ kmer/kmer.go
@@ -4,3 +4,19 @@ const (
 	MaxKSmall    int = 12
 	MaxKAbsolute int = 15
 )
+
+type KmerManager interface {
+	Init([]byte)
+	AddBase(byte)
+}
+
+type KmerSlice interface {
+	Len() int
+	Less(int, int)
+	Swap(int, int)
+}
+
+type KmerCounter interface {
+	Count([]byte)
+	Finish()
+}
