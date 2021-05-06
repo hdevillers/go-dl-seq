3623d28f6c40249fa72354fae56744eb46d2bda8
Hugo Devillers <hugo.devillers@gmail.com>
2021-05-06 14:28:17 +0200

    Multiple channel to manager multiple input files in small counter.

--- kmer/csmall.go
+++ kmer/csmall.go
@@ -7,12 +7,13 @@ import (
 )
 
 type csmall struct {
-	Km *Kmer32  // Kmer manager
-	C  []uint64 // Kmer counter
-	F  bool     // Finished indicator
+	Km *Kmer32    // Kmer manager
+	C  [][]uint64 // Kmer counter
+	F  bool       // Finished indicator
+	L  int        // Counter indice (multiple channel)
 }
 
-func NewCsmall(k int) *csmall {
+func NewCsmall(k int, l int) *csmall {
 	// Do not instantiate for large k value
 	if k > MaxKSmall {
 		panic(errors.New("[KMER SMALL COUNTER]: Too large K value."))
@@ -22,8 +23,12 @@ func NewCsmall(k int) *csmall {
 	var cs csmall
 	cs.Km = NewKmer32(k)
 	n := math.Pow(4.0, float64(k))
-	cs.C = make([]uint64, int64(n))
+	cs.C = make([][]uint64, l)
+	for i := 0; i < l; i++ {
+		cs.C[i] = make([]uint64, int64(n))
+	}
 	cs.F = false
+	cs.L = 0
 
 	// Return the counter
 	return &cs
@@ -34,13 +39,17 @@ func (cs *csmall) Count(b []byte) {
 	cs.Km.Init(b)
 
 	// Count the first word
-	cs.C[cs.Km.W]++
+	cs.C[cs.L][cs.Km.W]++
 
 	// Count the following words
 	for i := int(cs.Km.K); i < len(b); i++ {
 		cs.Km.AddBase(b[i])
-		cs.C[cs.Km.W]++
+		cs.C[cs.L][cs.Km.W]++
 	}
+}
+
+func (cs *csmall) NextChannel() {
+	cs.L++
 }
 
 func (cs *csmall) Finish() {
@@ -53,8 +62,12 @@ func (cs *csmall) PrintAll() {
 		panic(errors.New("[KMER SMALL COUNTER]: Before printing counted values, you must call the Finish method."))
 	}
 	k := int(cs.Km.K)
-	for i := 0; i < len(cs.C); i++ {
-		fmt.Printf("%s\t%d\n", Kmer32String(uint32(i), k), cs.C[i])
+	for i := 0; i < len(cs.C[0]); i++ {
+		fmt.Printf("%s", Kmer32String(uint32(i), k))
+		for j := 0; j < cs.L; j++ {
+			fmt.Printf("\t%d", cs.C[j][i])
+		}
+		fmt.Printf("\n")
 	}
 }
 
@@ -63,9 +76,17 @@ func (cs *csmall) Print() {
 		panic(errors.New("[KMER SMALL COUNTER]: Before printing counted values, you must call the Finish method."))
 	}
 	k := int(cs.Km.K)
-	for i := 0; i < len(cs.C); i++ {
-		if cs.C[i] != 0 {
-			fmt.Printf("%s\t%d\n", Kmer32String(uint32(i), k), cs.C[i])
+	for i := 0; i < len(cs.C[0]); i++ {
+		sum := uint64(0)
+		for j := 0; j < cs.L; j++ {
+			sum += cs.C[j][i]
+		}
+		if sum > 0 {
+			fmt.Printf("%s", Kmer32String(uint32(i), k))
+			for j := 0; j < cs.L; j++ {
+				fmt.Printf("\t%d", cs.C[j][i])
+			}
+			fmt.Printf("\n")
 		}
 	}
 }
